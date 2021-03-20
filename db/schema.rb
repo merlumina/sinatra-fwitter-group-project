307afb4b7f6c084f432a21dc790249d848422dc4
Liz <lizriderwilson@gmail.com>
2021-03-20 11:31:37 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_152306) do
+ActiveRecord::Schema.define(version: 2021_03_18_170940) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
